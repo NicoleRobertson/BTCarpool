86a9c01f556ab804dc92ec84f0ad0ecb171ad64f
erikstannemark <erikstannemark@gmail.com>
2021-08-27 16:22:58 +0200

    added detailspage

--- src/main/java/demo/BTCarpool/BtCarpoolController.java
+++ src/main/java/demo/BTCarpool/BtCarpoolController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -20,5 +21,13 @@ public class BtCarpoolController {
 
         return "startpage";
     }
+
+   /* @GetMapping("/{id}")
+    public String recipe(Model model, @PathVariable int id) {
+        Recipe recipe = repository.getRecipe(id);
+        model.addAttribute("recipe", recipe);
+
+        return "Recipe";
+    } */
 
 }
